980e1828df2116d2a8aad631133cfe071c720d0e
AddisuAman <addisuaman11@gmail.com>
2023-01-24 07:52:09 -0800

    main.h

--- main.h
+++ main.h
@@ -104,4 +104,13 @@ unsigned int print_string_width(buffer_t *output,
 unsigned int print_neg_width(buffer_t *output, unsigned int printed,
 		unsigned char flags, int wid);
 
+/* Helper Functions */
+buffer_t *init_buffer(void);
+void free_buffer(buffer_t *output);
+unsigned int _memcpy(buffer_t *output, const char *src, unsigned int n);
+unsigned int convert_sbase(buffer_t *output, long int num, char *base,
+		unsigned char flags, int wid, int prec);
+unsigned int convert_ubase(buffer_t *output, unsigned long int num, char *base,
+		unsigned char flags, int wid, int prec);
+
 #endif
